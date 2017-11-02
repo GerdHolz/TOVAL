a9410f28237fa6056dabaae11868bcae7f16f28f
Thomas Stocker <thomas.stocker@de.markant.com>
2017-11-02 21:23:30 +0100

    Added Javadoc in class IFileExporter

--- src/de/invation/code/toval/file/IFileExporter.java
+++ src/de/invation/code/toval/file/IFileExporter.java
@@ -2,14 +2,39 @@ package de.invation.code.toval.file;
 
 import java.io.File;
 
+/**
+ * Interface for file exporters.<br>
+ * <br>
+ * Used e.g. for excel and word exporters (poiutils).
+ *
+ */
 public interface IFileExporter {
 	
+	/**
+	 * Returns the output file (i.e. file which is used to write contents).<br>
+	 * @return
+	 */
 	public File getOutputFile();
 	
-	public String getFileName();
+	/**
+	 * Returns the file name part of the output file.
+	 * @return
+	 */
+	default String getFileName(){
+		return FileUtils.getFile(getOutputFile());
+	}
 	
+	/**
+	 * Creates the output file.
+	 * @throws Exception
+	 */
 	public void createFile() throws Exception;
 	
+	/**
+	 * Indicates whether there is content to export.<br>
+	 * Can be used to abort file creation when there is no content.
+	 * @return {@code true} when there is content to export; {@code false} otherwise.
+	 */
 	public boolean contentsToExport();
 
 }
